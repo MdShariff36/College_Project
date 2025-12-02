658b1fffc1141fdea1c428a7fef256437eee5bf8
MdShariff36 <shariffchatgpt@gmail.com>
2025-12-02 15:48:34 +0530

    Create utils.js

---
+++ frontend/assets/js/utils.js
@@ -0,0 +1,10 @@
+// small helpers used across pages
+function getToken(){ return localStorage.getItem('token'); }
+function saveToken(t){ localStorage.setItem('token', t); }
+function clearToken(){ localStorage.removeItem('token'); }
+function authHeader(){ const t = getToken(); return t? {'Authorization':'Bearer '+t} : {}; }
+
+function handleFetchErrors(res){
+  if(!res.ok) return res.text().then(t=>{ throw new Error(t || res.statusText); });
+  return res.json();
+}
